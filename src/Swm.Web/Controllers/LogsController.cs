24b111ada3ddd66b3deb0c555af44dac7cc633e9
wangjianjun <1176398319@qq.com>
2021-01-03 08:46:24 +0800

    重新组织文件

---
+++ src/Swm.Web/Controllers/LogsController.cs
@@ -0,0 +1,120 @@
+﻿// Copyright 2020 王建军
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Arctic.NHibernateExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swm.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        readonly LogDbContext _context;
+
+        public LogsController(LogDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 日志列表
+        /// </summary>
+        /// <param name="args">查询参数</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ListResult<LogEntry>> Get([FromQuery]LogListArgs args)
+        {
+            var pagedList = await SearchLogsAsync(_context.Logs.AsNoTracking(), args.Filter, args.Sort, args.Current, args.PageSize);
+
+            foreach (var item in pagedList.List)
+            {
+                item.Time = DateTime.SpecifyKind(item.Time, DateTimeKind.Local);
+            }
+
+            return new ListResult<LogEntry>
+            {
+                Success = true,
+                Data = pagedList.List,
+                Total = pagedList.Total,
+            };
+        }
+
+        /// <summary>
+        /// 跟踪日志
+        /// </summary>
+        /// <param name="args">跟踪参数</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("trace")]
+        public async Task<ListResult<LogEntry>> Trace([FromQuery]LogTraceArgs args)
+        {
+            var pagedList = await SearchLogsAsync(_context.Logs.AsNoTracking(), args.Filter, args.Sort, args.Current, args.PageSize);
+
+            foreach (var item in pagedList.List)
+            {
+                item.Time = DateTime.SpecifyKind(item.Time, DateTimeKind.Local);
+            }
+
+            return new ListResult<LogEntry>
+            {
+                Success = true,
+                Data = pagedList.List,
+                Total = pagedList.Total,
+            };
+        }
+
+        private static async Task<PagedList<LogEntry>> SearchLogsAsync(IQueryable<LogEntry> q, Func<IQueryable<LogEntry>, IQueryable<LogEntry>> filter, string? sort, int? current, int? pageSize)
+        {
+            if (current == null || current.Value < 1)
+            {
+                current = 1;
+            }
+            if (pageSize == null || pageSize.Value < 1)
+            {
+                pageSize = 20;
+            }
+            q = filter(q);
+
+            if (!string.IsNullOrWhiteSpace(sort))
+            {
+                q = q.OrderBy(sort);
+            }
+
+            var totalItemCount = q.Count();
+
+            if (totalItemCount == 0)
+            {
+                return new PagedList<LogEntry>(new List<LogEntry>(), 1, pageSize.Value, 0);
+            }
+
+            int start = (current.Value - 1) * pageSize.Value;
+            var list = await q.Skip(start)
+                .Take(pageSize.Value)
+                .ToListAsync()
+                .ConfigureAwait(false);
+            return new PagedList<LogEntry>(list, 1, pageSize.Value, 0);
+
+        }
+    }
+}
---
+++ src/Swm.Web/Models/LogTraceArgs.cs
@@ -0,0 +1,119 @@
+﻿// Copyright 2020 王建军
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Swm.Web.Controllers
+{
+    /// <summary>
+    /// 日志跟踪参数
+    /// </summary>
+    public class LogTraceArgs
+    {
+        /// <summary>
+        /// 查询开始时间
+        /// </summary>
+        public DateTime TimeFrom { get; set; }
+
+        /// <summary>
+        /// 取多少秒
+        /// </summary>
+        public int? Seconds { get; set; }
+
+        /// <summary>
+        /// 计算得到的查询结束时间
+        /// </summary>
+        internal DateTime? TimeTo
+        {
+            get
+            {
+                if (this.Seconds.HasValue)
+                {
+                    return this.TimeFrom.AddSeconds(Seconds.Value);
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 请求Id
+        /// </summary>
+        public string? RequestId { get; set; }
+
+        /// <summary>
+        /// 要查询的关键字
+        /// </summary>
+        public string? Keyword { get; set; }
+
+        /// <summary>
+        /// 在查询对象上应用筛选条件
+        /// </summary>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public IQueryable<LogEntry> Filter(IQueryable<LogEntry> q)
+        {
+            RequestId = trim(RequestId);
+            Keyword = trim(Keyword);
+
+            if (RequestId != null)
+            {
+                q = q.Where(x => x.RequestId == RequestId);
+            }
+
+            if (!string.IsNullOrWhiteSpace(Keyword))
+            {
+                q = q.Where(x => x.Message.Contains(Keyword));
+            }
+
+            q = q.Where(x => x.Time >= TimeFrom);
+
+            if (TimeTo != null)
+            {
+                q = q.Where(x => x.Time <= TimeTo);
+            }
+
+            return q;
+
+            static string? trim(string? str)
+            {
+                str = str?.Trim();
+                if (string.IsNullOrEmpty(str))
+                {
+                    str = null;
+                }
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string? Sort { get; set; }
+
+        /// <summary>
+        /// 基于 1 的当前页面，默认值为 1。
+        /// </summary>
+        public int? Current { get; set; } = 1;
+
+        /// <summary>
+        /// 每页大小，默认值为 10。
+        /// </summary>
+        public int? PageSize { get; set; }
+
+    }
+
+
+}
